54d3da2074f602adad025fe607e1adbcb26b1c6d
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-26 13:12:08 +0200

    edit popular component to shop page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/popular/Popular";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   );
 };
