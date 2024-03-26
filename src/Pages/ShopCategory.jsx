d265dcd6b75884f4963076113b48093d7140c592
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-26 16:13:49 +0200

    edit banner for Pages

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
 
-const ShopCategory = () => {
-  return <div className="shop-category"></div>;
+const ShopCategory = (props) => {
+  const { all_products } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img src={props.banner} alt="" />
+    </div>
+  );
 };
 
 export default ShopCategory;
