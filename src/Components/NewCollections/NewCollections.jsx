ab8cf674dbacdd676eb8456848df19ddfad0ab27
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-26 15:26:37 +0200

    move assits into components

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./NewCollections.css";
-import new_collection from "../../Assets/new_collections";
+import new_collection from "../Assets/new_collections";
 import Item from "../item/Item";
 
 const NewCollections = () => {
--- src/Components/popular/Popular.jsx
+++ src/Components/popular/Popular.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./popular.css";
-import data_product from "../../Assets/data";
+import data_product from "../Assets/data";
 import Item from "../item/Item";
 
 const Popular = () => {
