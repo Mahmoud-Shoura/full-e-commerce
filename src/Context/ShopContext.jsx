7961fe953120c14cea51c66119dc6aae7a0f7c90
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-28 00:56:04 +0200

    edit fun to productDisplay

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -14,6 +14,7 @@ const ShopContextProvider = (props) => {
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log(cartItems);
   };
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
