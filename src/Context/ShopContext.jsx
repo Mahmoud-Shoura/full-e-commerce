69a07757017a4ab73a1e15524aabcaace5982d91
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-28 00:52:15 +0200

    edit addtocart & removefromcart fun

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,10 +1,24 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_products from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
-
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_products.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
 const ShopContextProvider = (props) => {
-  const contextValue = { all_products };
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+  const contextValue = { all_products, cartItems, addToCart, removeFromCart };
 
   return (
     <ShopContext.Provider value={contextValue}>
